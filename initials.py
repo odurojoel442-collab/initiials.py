132da9a16c68cf3b7eafbc67577664f9f7e24b96
odurojoel442-collab <odurojoel442@gmail.com>
2026-05-31 02:57:02 +0000

    Create initials.py

---
+++ initials.py
@@ -0,0 +1,32 @@
+# File: initials.py
+# Description: Print out my initials in stylized large letters.
+# Assignment Number: 1
+#
+# Name: oduro Joel
+# STUDENT ID: 2425402997
+# Email: 2425402997@live.gctu.edu.gh
+# Grader: carolyn
+#
+# On my honor, Oduro Joel, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
+
+def main():
+# This program prints my initials in styled large letters
+    print()
+    print("...OJO")
+    print()
+    print("...  OOOOOOOO  ........      JJJJJJ........  OOOOOOOO  .....")
+    print("... OO      OO ........      JJJJJJ........ OO      OO .....")
+    print("...OO        OO........          JJ........OO        OO.....")
+    print("...OO        OO........          JJ........OO        OO.....")
+    print("...OO        OO........          JJ........OO        OO.....")
+    print("...OO        OO........          JJ........OO        OO.....")
+    print("...OO        OO........JJ        JJ........OO        OO.....")
+    print("...OO        OO........JJ        JJ........OO        OO.....")
+    print("... OO      OO ...**...JJJJJJJJJJJJ...**... OO      OO ...**")
+    print("...  OOOOOOOO  ...**... JJJJJJJJJJ ...**...  OOOOOOOO  ...**")
+    print()
+
+
+main()
